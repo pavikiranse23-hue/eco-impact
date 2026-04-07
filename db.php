f79e01490ab309f0f7a1652af7970f68917b9e32
Pavikiran K <pavikiran.se23@bitsathy.ac.in>
2026-04-07 11:03:50 +0530

    db changed to online 2

--- db.php
+++ db.php
@@ -1,7 +1,19 @@
 <?php
-$conn = mysqli_connect("localhost","root","","eco_impact");
+
+
+
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if(!$conn){
     die("Database Connection Failed");
 }
+
+
 ?>
